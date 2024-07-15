e919f8f7fd75e148e3e8fcc59b1c585b1dd40bcf
Svengineering <info@viaclara.io>
2024-07-15 04:54:14 +0200

    initial commit, simple script that enables maintenance mode for non-logged in users

---
+++ my-maintenance-plugin.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Plugin Name: Mein Maintenance-Modus
+ * Description: Maintenance-Modus einschalten
+ */
+
+
+defined( 'ABSPATH' ) || exit;
+
+
+
+/**
+ * Maintenance message when active
+*/
+function maintenance_mode()
+{
+  nocache_headers();
+  if ( !is_user_logged_in() ) {
+  wp_die( 
+  	'<h1>Wartung</h1>', 
+	  ['response' => '503']);
+  }
+}
+add_action('parse_request', 'maintenance_mode');
+
+
